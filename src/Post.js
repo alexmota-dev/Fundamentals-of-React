2fa09bf64c6b1a4f6ec407c76f47ea1c52974f20
AlexxMotta <alexmota@alu.ufc.br>
2023-04-16 12:35:42 -0300

    Post com props e propTypes obrigatorios

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const Post = (props) => {
   return (
     <div>
-        <article>
-            <strong>{props.title}</strong><br/>
-            <small>{props.subtitle}</small><br/>
-        </article>
-    </div>
+      <article>
+          <strong>{props.post.title}</strong><br/>
+          <small>{props.post.subtitle}</small><br/>
+          Media: {props.likes / 2}
+          <br/>
+      </article>
+  </div>
   )
 }
 
-export default Post
+Post.propTypes ={
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Post;
