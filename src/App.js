96aad261c019ac9c3da22e15f612a7df98acb3a8
AlexxMotta <alexmota@alu.ufc.br>
2023-05-09 19:07:31 -0300

    usando contextAPI para usar o theme de qualquer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, createContext} from "react";
 import Header from "./Header";
 import Post from "./Post";
 
+export const ThemeContext = createContext('dark');
+
 function App(){
+    const [theme, setTheme] = useState('dark');
     const [posts, setPosts] = useState([
         {id:Math.random(),title:"Title#01", subtitle:"Sub#01", likes:21, read: true},
         {id:Math.random(),title:"Title#02", subtitle:"Sub#02", likes:23, read: false},
         {id:Math.random(),title:"Title#03", subtitle:"Sub#03", likes:18, read: true},
         {id:Math.random(),title:"Title#04", subtitle:"Sub#04", likes:20, read: false},
     ]);
+
+    function handleToggleTheme(){
+        setTheme((prevState)=> prevState == 'dark'
+        ? 'light'
+        : 'dark');
+    }
 
     function handleRefresh(){
         console.log("click");
@@ -30,9 +39,14 @@ function App(){
         ))
     }
     return (
-        <React.Fragment>
-            <Header title="Posts da Semana">
-                <button onClick={handleRefresh}>Atualizar</button>
+        <ThemeContext.Provider value = {theme}>
+            <Header
+                onToggleTheme={handleToggleTheme}
+                title="Posts da Semana"
+                theme = {theme}>
+                <button onClick={handleRefresh}>
+                    Atualizar
+                </button>
             </Header>
 
             <hr/>
@@ -43,9 +57,10 @@ function App(){
                     likes={post.likes}
                     post={post}
                     onRemove={handleRemove}
+                    theme ={theme}
                 />
             ))}
-        </React.Fragment>
+        </ThemeContext.Provider>
     )
   }
 
