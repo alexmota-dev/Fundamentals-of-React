422d80770b9c4f0a4806d6be4cfdb8eb95f93c2c
AlexxMotta <alexmota@alu.ufc.br>
2023-05-09 19:08:18 -0300

    quebra de linha simples

--- src/PostHeader.js
+++ src/PostHeader.js
@@ -9,7 +9,8 @@ const PostHeader = (props) => {
             {props.post.read && <s>{props.post.title}</s>}
             {!props.post.read && props.post.title}
         </strong>
-        <Button onClick={()=> props.onRemove(props.post.id)}>
+        <Button
+          onClick={()=> props.onRemove(props.post.id)}>
             Remover
         </Button>
     </>
