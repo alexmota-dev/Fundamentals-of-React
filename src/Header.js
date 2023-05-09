d5e541a127d9e32b85f01e081d870dc0a954d6ab
AlexxMotta <alexmota@alu.ufc.br>
2023-05-09 19:07:56 -0300

    atualizando header, com button de mudar tema

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 const Header = (props) => {
   return (
     <div>
         <h1>{props.title}</h1>
-        <h2>{Math.random()}</h2>
+        <Button
+          onClick={props.onToggleTheme}>
+          Mudar Tema
+        </Button>
         {props.children}
     </div>
   )
 }
+Header.propTypes = {
+  onToggleTheme: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
 
 Header.defaultProps = {
   title:`Alek a lenda`
