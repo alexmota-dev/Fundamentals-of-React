47297c5256611045663126e3bd4911760d5c91c7
AlexxMotta <alexmota@alu.ufc.br>
2023-05-09 19:06:46 -0300

    criando button com tema

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types';
+import { ThemeContext } from './App';
 
 const Button = (props) => {
+  const theme = useContext(ThemeContext);
   return (
-    <button onClick={()=> props.onClick()}>
-        {props.children}
+    <button
+      onClick={()=> props.onClick()}
+      style={{
+        color: theme == "dark" ? "#fff" : "#000",
+        background: theme == "dark" ? "#000" : "#fff",
+      }}
+    >
+      {props.children}
     </button>
   )
 }
