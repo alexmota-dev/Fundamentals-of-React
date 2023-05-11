f6aea77836d4db3b1f0d2ba76b2ed2834d20ac25
AlexxMotta <alexmota@alu.ufc.br>
2023-05-11 11:15:18 -0300

    alterando a importação do Theme para o ThemeContext

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import PropTypes from 'prop-types';
-import { ThemeContext } from './App';
+import { ThemeContext } from './ThemeContext';
 
 const Button = (props) => {
   const {theme} = useContext(ThemeContext);
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React, { useContext }from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
-import { ThemeContext } from './App'
+import { ThemeContext } from './ThemeContext'
 
 
 const Header = (props) => {
