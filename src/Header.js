8e210eaa68166f7c509579419df943090326fadd
Alex Mota <alexmota@alu.ufc.br>
2023-05-18 15:03:07 -0300

    importando css no header e no index

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,14 @@ import React, { useContext }from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 import { ThemeContext } from './ThemeContext'
-
+import "./Header.css";
 
 const Header = (props) => {
   const {onToggleTheme} = useContext(ThemeContext);
 
   return (
     <div>
-        <h1>{props.title}</h1>
+        <h1>{props.title}</h1> {/* refatorar essa linha */}
         <Button
           onClick={onToggleTheme}>
           Mudar Tema
